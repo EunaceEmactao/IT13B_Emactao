f02dcb658e9ab0f4d96a29e226b0776736cb4420
Eunace Faith Emactao <kimeunna.917@gmail.com>
2025-02-13 11:32:28 +0800

    Added HelloWorld class

--- Emactao_IT13B_activities/src/Prelim/Lab_act1_Arithmetics.java
+++ Emactao_IT13B_activities/src/Prelim/Lab_act1_Arithmetics.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Prelim;
 
-/**
- *
- * @author User
- */
+import java.util.Scanner;
+
 public class Lab_act1_Arithmetics {
-    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input the first number = ");
+        int myFirst;
+        myFirst = in.nextInt();
+
+        System.out.println("Input the second number = ");
+        int mySecond;
+        mySecond = in.nextInt();
+
+        System.out.println("Input the third number = ");
+        int myThird;
+        myThird = in.nextInt();
+
+        int myFourth = 3;
+
+        System.out.println(myFirst + " * " + mySecond + " " + "+" + " " + myThird + " = " + "" + ((myFirst * mySecond) + myThird));
+        System.out.println("(" + myFirst + " - " + mySecond + ")" + " % " + "" + myThird + " = " + "" + ((myFirst - mySecond) % myThird));
+        System.out.println("(" + myFirst + " " + "+" + " "+ mySecond + " " + "+" + " " + myThird + ")" + "/" + myFourth + " " + "=" + " " + ((myFirst + mySecond + myThird) / myFourth));
+        System.out.println(myFirst + " " + "*" + " " + myThird + " " + "-" + " " + "(" + mySecond + " " + "*" + " "+ mySecond + ")" +  " " + "=" + " " + (myFirst * myThird - mySecond * mySecond));
+    }
 }
