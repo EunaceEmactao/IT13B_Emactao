bedc435846340b9c5404544f408d8fd4b674033f
Eunace Emactao <kimeunna.917@gmail.com>
2025-04-09 09:46:02 +0800

    assignment

--- Emactao_IT13B_activities/src/midterm/EncryptFileHandling.java
+++ Emactao_IT13B_activities/src/midterm/EncryptFileHandling.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package midterm;
 
 import java.io.File;
@@ -13,24 +10,25 @@ import java.util.Scanner;
 public class EncryptFileHandling {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        File myFile = new File("C:\\Users\\Eunace Faith Emactao\\OneDrive\\Desktop\\EmactaoFile.txt");
+        try (FileWriter myWriter = new FileWriter("C:\\Users\\Eunace Faith Emactao\\OneDrive\\Desktop\\EmactaoInput1.txt")) {
+            myWriter.write("I love you!\nGwapa ko!\nBuotan si Ma'am.");
+        }
+
+        File myFile = new File("C:\\Users\\Eunace Faith Emactao\\OneDrive\\Desktop\\EmactaoInput1.txt");
         Scanner in = new Scanner(myFile);
+
         System.out.println("Encrypted Message:");
         while (in.hasNextLine()) {
             String data = in.nextLine();
-
             int key = 6;
             String message = data;
-
             String encryptedMessage = encryptMessage(message, key);
-
             System.out.println(encryptedMessage);
 
-            FileWriter myWriter = new FileWriter("C:\\Users\\Eunace Faith Emactao\\OneDrive\\Desktop\\Encrypted.txt");
-            myWriter.write(encryptMessage(message, key));
-            myWriter.close();
+            try (FileWriter myWriter = new FileWriter("C:\\Users\\Eunace Faith Emactao\\OneDrive\\Desktop\\EmactaoEncrypted1.txt", true)) {
+                myWriter.write(encryptedMessage + "\n");
         }
-    }
+    }}
 
     public static String encryptMessage(String message, int key) {
         char[] chars = message.toCharArray();
